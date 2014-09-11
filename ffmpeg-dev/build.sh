17acf97d65824ce7f25aa62edb2ad51b3319d531
Peter <peter@lindap-mac80114.biol.ucl.ac.uk>
2014-09-11 20:52:11 +0100

    changes to make recipes work with Mac OSX

--- ffmpeg-dev/build.sh
+++ ffmpeg-dev/build.sh
@@ -1,3 +1,13 @@
-./configure --vars ASPELL="$PREFIX/bin/aspell"
-make
-make install
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform      
+  	echo "Darwin"
+    # brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
+    cp -R /usr/local/Cellar/ffmpeg/2.3.3/lib/ $PREFIX/lib;
+    cp -R /usr/local/Cellar/ffmpeg/2.3.3/include $PREFIX/include;
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+	echo "Linux"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+	echo "Windows"
+fi
--- sox/build.sh
+++ sox/build.sh
@@ -1,3 +1,9 @@
-./configure --vars ASPELL="$PREFIX/bin/aspell"
-make
-make install
+mkdir $PREFIX/bin
+cp $SRC_DIR/sox  $PREFIX/bin
+chmod +x $PREFIX/bin/sox
+cp $SRC_DIR/rec  $PREFIX/bin
+chmod +x $PREFIX/bin/rec
+cp $SRC_DIR/soxi $PREFIX/bin
+chmod +x $PREFIX/bin/soxi
+cp $SRC_DIR/play $PREFIX/bin
+chmod +x $PREFIX/bin/play
