f2f4e5fd418b9915b2b990ec748e7b1a3e0eba5f
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2016-01-20 18:32:53 +0000

    fixed packaging in windows

--- flyclimbing/bld.bat
+++ flyclimbing/bld.bat
@@ -1,6 +1,6 @@
-REM python setup.py build
-REM python setup.py install
-REM if errorlevel 1 exit 1
+python setup.py build
+python setup.py install
+if errorlevel 1 exit 1
 
-REM rd /s /q %SP_DIR%\__pycache__
-REM if errorlevel 1 exit 1
+rd /s /q %SP_DIR%\__pycache__
+if errorlevel 1 exit 1
